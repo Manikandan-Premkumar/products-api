f5ffa68e122756a98f77575f6528e0eb4899fd99
P Manikandan <manikandanpremkumar@outlook.com>
2026-06-29 04:19:49 +0530

    new1

--- database.py
+++ database.py
@@ -11,11 +11,11 @@ def get_connection():
     if database_url:
         return psycopg.connect(database_url)
 
-    return conn = psycopg.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        sslmode="require"
-    )
+    return psycopg.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    sslmode="require"
+)
